d41bb23bbad50af383004207c6ff5c4c7ea9ebde
유림 한 <y090724@naver.com>
2022-01-15 03:03:51 +0900

    한유림 : 커밋 연습

---
+++ HomeWork/HomeWork_220114_ToUpper/HomeWork_220114_ToUpper.cpp
@@ -0,0 +1,18 @@
+﻿// HomeWork_220114_ToUpper.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void ToUpper(const char* _Left, char* _Right)
+{
+	_Right[] = _Left[];
+}
+
+int main()
+{
+	char Text[5];
+
+	ToUpper("pP1231dcwsec", Text);
+	//      "12ABCCEEE" -> 알파벳소문자가 대문자가 되도록 바꾼다.
+	printf_s(Text);
+}
