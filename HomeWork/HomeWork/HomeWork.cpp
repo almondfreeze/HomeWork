3c51b6a8994a6addc571ba8190a8963e91b8c5ec
유림 한 <y090724@naver.com>
2022-01-14 14:58:07 +0900

    한유림 : 숙제 커밋 연습

---
+++ HomeWork/HomeWork/HomeWork.cpp
@@ -0,0 +1,18 @@
+﻿// HomeWork.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void ToUpper(const char* _Left, char* _Right)
+{
+
+}
+
+int main()
+{
+	char Text[100];
+
+	ToUpper("12abCcEee", Text);
+	//      "12ABCCEEE"
+	printf_s(Text);
+}
