6938c60dfbb49c92fb9ae56a9e2022e24fe60086
유림 한 <y090724@naver.com>
2022-01-16 18:39:53 +0900

    한유림 : 220114 숙제 커밋

--- HomeWork/HomeWork_220114_ToUpper/HomeWork_220114_ToUpper.cpp
+++ HomeWork/HomeWork_220114_ToUpper/HomeWork_220114_ToUpper.cpp
@@ -5,14 +5,29 @@
 
 void ToUpper(const char* _Left, char* _Right)
 {
-	_Right[] = _Left[];
+	
+
+	for (int LeftValue = 0; 0 != _Left[LeftValue]; LeftValue += 1)
+	{
+		_Right[LeftValue] = _Left[LeftValue];
+
+		if (_Left[LeftValue] > 96)
+		{
+			_Right[LeftValue] = _Left[LeftValue] - 32;
+		}
+	}
+
 }
 
 int main()
 {
-	char Text[5];
+	char Alphabet = 'z';
+	// a = 97       A = 65   32
+	// h = 104      H = 72   32
 
-	ToUpper("pP1231dcwsec", Text);
+	char Text[100];
+
+	ToUpper("12adefa", Text);
 	//      "12ABCCEEE" -> 알파벳소문자가 대문자가 되도록 바꾼다.
 	printf_s(Text);
 }
